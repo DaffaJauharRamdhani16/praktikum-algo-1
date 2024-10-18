7658c0386701d5d385103ea0c3ffb72435e9fea8
DaffaJauharRamdhani16 <2403010102@unper.ac.id>
2024-10-18 14:03:04 +0700

    menambahkan file ex.py

--- ex.py
+++ ex.py
@@ -1,3 +1,10 @@
-a = input ('masukan angka pertama')
-b = input ('masukan angka kedua')
-
+nama = 'daffa jauhar ramdhani'
+alamat = 'tasikmalaya'
+mahasiswa = 'true'
+tinggi_badan = 169.5
+usia = 19
+gender = 'male'
+hoby = 'olahraga'
+print ( 'nama saya : ' + nama + ' alamat saya di ' + alamat + ' status mahasiswa ' + mahasiswa )
+print ( ' tinggi saya adalah ' + str(tinggi_badan) + ' cm ' + ' usia saya adalah ' + str(usia) + ' tahun ')
+print ( 'gender saya adalah ' + gender + 'hoby saya adalah ' + hoby)
